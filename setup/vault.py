efd164fed2edb64809c2de7f1b279c40e4857a94
Kevin Coakley <kcoakley@sdsc.edu>
2015-07-27 11:47:39 -0700

    A script for managing the user's credentials vault

---
+++ setup/vault.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import argparse
+from ucsd_bigdata.vault import Vault
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Script to manage the system credentials vault.")
+    parser.add_argument('--set', dest='set', action='store', type=str, default=None,
+                        help='Enter the path to the vault you would like to use.')
+    parser.add_argument('--history', dest='history', action='store_true', default=False,
+                        help='Select a vault from your vault history.')
+
+    args = vars(parser.parse_args())
+
+    vault = Vault()
+
+    if args["set"] is not None:
+        vault.set(args["set"])
+    elif args["history"]:
+        vault_history = vault.history()
+
+        for position, history_path in enumerate(vault_history):
+            print "\t%s. %s" % (position + 1, history_path)
+
+        # Make sure user_input is valid
+        selected_vault = None
+        while selected_vault is None:
+            user_input = raw_input("\nEnter the number next to the vault that you "
+                                   "would like to use: ")
+            try:
+                if 0 < int(user_input) <= len(vault_history):
+                    selected_vault = int(user_input) - 1
+            except ValueError:
+                continue
+
+        vault.set(vault_history[selected_vault])
+
+    print "\nThe Vault directory is: %s" % vault.path
---
+++ ucsd_bigdata/vault.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+
+
+class Vault:
+
+    def __init__(self):
+        self.home = os.environ['HOME']
+        self.vault_config = "%s/.vault" % self.home
+        self.path = Vault.get(self)
+
+    def get(self):
+        path = "%s/Vault" % self.home
+
+        if os.path.isfile(self.vault_config):
+            try:
+                with open(self.vault_config, 'r') as f:
+                    vault_config_json = json.load(f)
+                    path = vault_config_json["vault"]
+            except ValueError:
+                print "%s is not valid json" % self.vault_config
+                sys.exit()
+
+        return path
+
+    def set(self, path):
+        vault_config_json = dict()
+
+        if os.path.isfile(self.vault_config):
+            try:
+                with open(self.vault_config, 'r') as f:
+                    vault_config_json = json.load(f)
+            except ValueError:
+                print "%s is not valid json" % self.vault_config
+                sys.exit()
+
+        vault_config_json["vault"] = path
+
+        if "history" in vault_config_json:
+            if path not in vault_config_json["history"]:
+                vault_config_json["history"].append(path)
+        else:
+            vault_config_json["history"] = []
+            vault_config_json["history"].append(path)
+
+        with open(self.vault_config, 'w') as f:
+            json.dump(vault_config_json, f, sort_keys=True, indent=4, separators=(',', ': '))
+
+        if not os.path.isdir(path):
+            try:
+                os.makedirs(path)
+            except Exception, e:
+                print "Error creating vault: %s : %s" % (path, e)
+                sys.exit()
+
+        self.path = Vault.get(self)
+
+    def history(self):
+        vault_config_json = dict()
+        history = []
+
+        if os.path.isfile(self.vault_config):
+            try:
+                with open(self.vault_config, 'r') as f:
+                    vault_config_json = json.load(f)
+            except ValueError:
+                print "%s is not valid json" % self.vault_config
+                sys.exit()
+
+        if "history" in vault_config_json:
+            history = vault_config_json["history"]
+
+        return history
